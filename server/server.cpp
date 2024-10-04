48c7a811372b637f7cb04f05527fd4275293e3b3
Ismail Benaaitona <ibenaait@e1r6p6.1337.ma>
2024-10-04 22:13:49 +0100

    n

--- server/server.cpp
+++ server/server.cpp
@@ -6,7 +6,7 @@
 /*   By: ibenaait <ibenaait@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 17:28:00 by mfadil            #+#    #+#             */
-/*   Updated: 2024/10/04 19:03:24 by ibenaait         ###   ########.fr       */
+/*   Updated: 2024/10/04 22:09:05 by ibenaait         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,26 @@ Server::Server(int port, std::string password) :_port(port), _password(password)
 	
 }
 
-Server::~Server() {}
+// Server::~Server() {
+
+// 	std::map<std::string,Channel*>::iterator it = _channels.begin();
+// 	while (it != _channels.end())
+// 	{
+// 		if(it->second)
+// 			delete it->second;
+// 		it++;
+// 	}
+// 	_channels.clear();
+// 	std::map<int,Client*>::iterator its = _clients.begin();
+// 	while (its != _clients.end())
+// 	{
+// 		if(its->second)
+// 			delete it->second;
+// 		its++;
+// 	}
+// 	_clients.clear();
+	
+// }
 
 // getters
 
@@ -191,10 +210,9 @@ std::vector<std::string> Server::splitCommands(std::string msg)
 
     while (std::getline(iss, part, ' ')) 
 	{
-		// std::cout << part.find(':') <<std::endl;
         if (part.find(':')  == 0) 
 		{
-            std::string restOfMessage = part.substr(1,part.size());
+            std::string restOfMessage = part.substr(1,part.size() -1);
             std::string remaining;
             if (std::getline(iss, remaining)) 
                 restOfMessage += " " + remaining;
@@ -206,7 +224,10 @@ std::vector<std::string> Server::splitCommands(std::string msg)
     }
     return cmd;
 }
-
+void Channel::cleatTopic()
+{
+	topic.clear();
+}
 std::map<int,Client*>::iterator	Server::findClientIt(int fd)
 {
 	std::map<int,Client*>::iterator ret = _clients.find(fd);
@@ -625,17 +646,7 @@ void Channel::setTime(std::string const &time)
 {
 	this->timeScCh = time;
 }
-Channel::~Channel()
-{
-	// std::map<Client*,bool>::iterator v = client.begin();
-	// while (v != client.end())
-	// {
-	// 	if(v->first)
-	// 		delete v->first;
-	// 	v++;
-	// }
-	// client.clear();
-}
+
 // std::string getTime()
 // {
 //         // Check if creatchannelTime is valid
@@ -722,6 +733,7 @@ Channel& Channel::operator=(const Channel& other) {
     }
     return *this;
 }
+
 bool	Channel::getLimit() const
 {
 	return islimit;
--- server/topic.cpp
+++ server/topic.cpp
@@ -1,10 +1,31 @@
 #include "../includes/irc.hpp"
+std::vector<std::string> splitCommandsT(std::string msg)
+{
+    std::vector<std::string> cmd;
+    std::string part;
+    std::istringstream iss(msg);
 
+    while (std::getline(iss, part, ' ')) 
+	{
+        if (part.find(':')  == 0) 
+		{
+            std::string restOfMessage = part.substr(0,part.size() - 1);
+            std::string remaining;
+            if (std::getline(iss, remaining)) 
+                restOfMessage += " " + remaining;
+            cmd.push_back(restOfMessage);
+            return cmd;
+        }
+		if(!part.empty())
+			cmd.push_back(part);
+    }
+    return cmd;
+}
 int    Server::TOPIC(std::string cmd, Client &c)
 {
     if(c.getState() != REGISTERED)
         return c.reply(ERR_NOTREGISTERED(c.getNickname(),c.getHost())),1;
-    std::vector<std::string> args = splitCommands(cmd);
+    std::vector<std::string> args = splitCommandsT(cmd);
     if(args.size() < 2)
         return c.reply(ERR_NEEDMOREPARAMS(c.getNickname(),c.getHost(),"TOPIC")),1;
     std::string target = args[1];
@@ -13,6 +34,8 @@ int    Server::TOPIC(std::string cmd, Client &c)
         return c.reply(ERR_NOSUCHCHANNEL(c.getHost(),c.getNickname(),target)),1;
     if(ch->checkIfIsClient(c.getNickname()) == 0)
         return c.reply(ERR_NOTONCHANNEL(c.getHost(),target)),1;
+    if(target.size() == 1 && target[0] == ':')
+        return ch->cleatTopic(),1;
     if(args.size() == 2)
     {
         if(!ch->getIsTopic())
