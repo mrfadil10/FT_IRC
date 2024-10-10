e65c4abb5b707d6534a2e7c82b4c544c3a80f517
Ismail Benaaitona <ibenaait@e1r6p6.1337.ma>
2024-10-10 23:57:53 +0100

    last

--- server/pass.cpp
+++ server/pass.cpp
@@ -1,25 +1,5 @@
 #include "../includes/irc.hpp"
 
-// std::string	ERR_PASSWDMISMATCH(Client &client)
-// {
-// 	return ("\033[1;91m" + client.getNickname() + " :Password incorrect\r\n");
-// }
-
-int		teststring(std::string test)
-{
-	const char *tst = test.c_str();
-	int		i = -1;
-	while (tst[++i])
-	{
-		if (tst[i] == '\r')
-		{
-			// std::cout << "character number : " << i << " Line Break\n";
-			return (1);
-		}
-	}
-	return (0);
-}
-
 int Server::cmdPass(std::string arg, Client &cl)
 {
 	
